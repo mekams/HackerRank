ce39c29fc2002a55eace9da91d519a3abb893a78
Kamran <kamraneg@gmail.com>
2022-10-09 02:37:43 +0530

    Task1

---
+++ Algorithms/Warmup/Triplet.js
@@ -0,0 +1,19 @@
+let a=[27,28,30]
+let b=[99,16,8]
+
+function triplets(a,b){
+    let i=0, alice=0, bob=0;
+    while(i <a.length && i< b.length ){
+        if(a[i]>b[i]){
+             alice+=1;
+             i++;
+        }else if(a[i]<b[i]){
+            bob+=1;
+            i++;
+        }else{
+            i++;
+        }
+    }
+    return [alice,bob]
+}
+console.log(triplets(a,b))
